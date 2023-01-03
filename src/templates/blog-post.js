71e3d52d3dc409042c00c0193ee49f329e03069c
Steve Gibbard <steve.gibbard@ons.gov.uk>
2023-01-03 15:24:17 +0000

    publish branch

---
+++ src/templates/blog-post.js
@@ -0,0 +1,207 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import readingTime from 'reading-time'
+
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import Tags from '../components/tags'
+import * as styles from './blog-post.module.css'
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost')
+    const previous = get(this.props, 'data.previous')
+    const next = get(this.props, 'data.next')
+    const plainTextDescription = documentToPlainTextString(
+      JSON.parse(post.description.raw)
+    )
+    const plainTextBody = documentToPlainTextString(JSON.parse(post.body.raw))
+    const { minutes: timeToRead } = readingTime(plainTextBody)
+
+    console.log("Raw Body :")
+    console.log(JSON.parse(post.body.raw))
+
+    const options = {
+      renderNode: {
+        [BLOCKS.EMBEDDED_ASSET]: (node) => {
+          console.log(node)
+          console.log(node.data)
+          
+          console.log(node.data.target.sys.id)
+          
+          const gImageData = getImage(node.data.target)
+          console.log(gImageData)
+
+          // This is an alternative work around for getting images in richText format
+          // to display
+          //const imageID = node.data.target.sys.id;
+          //const {
+          //  file: {url}, 
+          //  title
+          //} = post.body.references.find(({contentful_id: id}) => id === imageID);
+
+          //return <img src={url} alt={title} />
+
+          return(<GatsbyImage image={gImageData} alt={node.data.target.description} />)
+        },
+      },
+    };
+
+
+    return (
+      <Layout location={this.props.location}>
+        <Seo
+          title={post.title}
+          description={plainTextDescription}
+          image={`http:${post.heroImage.resize.src}`}
+        />
+        <Hero
+          image={post.heroImage?.gatsbyImage}
+          title={post.title}
+          content={post.description}
+        />
+        <div className={styles.container}>
+          <span className={styles.meta}>
+            {post.author?.name} &middot;{' '}
+            <time dateTime={post.rawDate}>{post.publishDate}</time> –{' '}
+            {timeToRead} minute read
+          </span>
+          <div className={styles.article}>
+            <div className={styles.body}>
+              {post.body?.raw && renderRichText(post.body, options)}
+            </div>
+            <Tags tags={post.tags} />
+            {(previous || next) && (
+              <nav>
+                <ul className={styles.articleNavigation}>
+                  {previous && (
+                    <li>
+                      <Link to={`/blog/${previous.slug}`} rel="prev">
+                        ← {previous.title}
+                      </Link>
+                    </li>
+                  )}
+                  {next && (
+                    <li>
+                      <Link to={`/blog/${next.slug}`} rel="next">
+                        {next.title} →
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </nav>
+            )}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug(
+    $slug: String!
+    $previousPostSlug: String
+    $nextPostSlug: String
+  ) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      slug
+      title
+      author {
+        name
+      }
+      publishDate(formatString: "MMMM Do, YYYY")
+      rawDate: publishDate
+      heroImage {
+        gatsbyImage(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
+        resize(height: 630, width: 1200) {
+          src
+        }
+      }
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            title
+            description
+            gatsbyImageData(width: 1000)
+            file {
+              url
+            }
+            __typename
+          }
+        }
+      }
+      tags
+      description {
+        raw
+      }
+    }
+    previous: contentfulBlogPost(slug: { eq: $previousPostSlug }) {
+      slug
+      title
+    }
+    next: contentfulBlogPost(slug: { eq: $nextPostSlug }) {
+      slug
+      title
+    }
+  }
+`
+/*
+export const pageQuery = graphql`
+  query BlogPostBySlug(
+    $slug: String!
+    $previousPostSlug: String
+    $nextPostSlug: String
+  ) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      slug
+      title
+      author {
+        name
+      }
+      publishDate(formatString: "MMMM Do, YYYY")
+      rawDate: publishDate
+      heroImage {
+        gatsbyImage(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
+        resize(height: 630, width: 1200) {
+          src
+        }
+      }
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+             contentful_id
+             title
+             file {
+                url
+             }
+          }
+       }
+      }
+      tags
+      description {
+        raw
+      }
+    }
+    previous: contentfulBlogPost(slug: { eq: $previousPostSlug }) {
+      slug
+      title
+    }
+    next: contentfulBlogPost(slug: { eq: $nextPostSlug }) {
+      slug
+      title
+    }
+  }
+`
+*/
