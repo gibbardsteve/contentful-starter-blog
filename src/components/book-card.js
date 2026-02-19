e4c79234d0ac98f8185089c09d9a597d3a3a41d1
Steve Gibbard <steve.gibbard@ons.gov.uk>
2026-02-19 21:36:26 +0000

    Add year to book-card.js

--- src/components/book-card.js
+++ src/components/book-card.js
@@ -75,7 +75,8 @@ const BookCard = ({ posts, type }) => {
     { value: 2022, label: '2022' },
     { value: 2023, label: '2023' },
     { value: 2024, label: '2024' },
-    { value: 2025, label: '2025' }
+    { value: 2025, label: '2025' },
+    { value: 2026, label: '2026' }
   ];
 
   // Filter the posts based on the rating (when selected)
